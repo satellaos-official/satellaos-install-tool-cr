8939c3b8c8d5b798567eea641e6ed3419c6f3fc7
Satella <forgottensatella@gmail.com>
2026-06-07 13:54:56 +0300

    Error Test

--- tree-installer-system/pictures/run.sh
+++ tree-installer-system/pictures/run.sh
@@ -10,6 +10,6 @@ fi
 
 Base="$REAL_HOME/satellaos-install-tool-cr/tree-installer-system"
 
-mkdir -p /usr/share/satellaos-core/pictures/
+sudo mkdir -p /usr/share/satellaos-core/pictures/
 
-rsync -ahP --chmod=D755,F644 "$Base/pictures/satellaos-sirius/" /usr/share/satellaos-core/pictures/
+sudo rsync -ahP --chmod=D755,F644 "$Base/pictures/satellaos-sirius/" /usr/share/satellaos-core/pictures/
